f0c1cd9182b79bea11ff0058953d8cee0547ec0e
Austris <austrisbaumanis@gmail.com>
2021-11-14 16:27:02 +0200

    added more options how to patternmatch days

--- lib/namedays/nameday_provider.ex
+++ lib/namedays/nameday_provider.ex
@@ -9,6 +9,12 @@ defmodule Namedays.NamedayProvider do
     get_init_content()
   end
 
+  def handle_cast({:get_date_nameday, %{month: month, day: day}}, state) do
+    get_namedays_by_listed_date([month, day], state)
+
+    {:noreply, state}
+  end
+
   def handle_cast({:get_date_nameday, nameday}, state) do
     Map.get(state, nameday)
     |> IO.inspect()
@@ -26,11 +32,7 @@ defmodule Namedays.NamedayProvider do
   def handle_cast(:get_todays_namedays, state) do
     {:ok, %DateTime{month: month, day: day}} = Application.fetch_env!(:namedays, :timezone) |> DateTime.now()
 
-    [month, day]
-      |> Enum.map(fn value -> Integer.to_string(value) |> String.pad_leading(2, "0") end)
-      |> Enum.reduce(fn (carry, elem) -> elem<>"-"<>carry end)
-      |> (&Map.get(state, &1)).()
-      |> IO.inspect()
+    get_namedays_by_listed_date([month, day], state)
 
     {:noreply, state}
   end
@@ -39,6 +41,7 @@ defmodule Namedays.NamedayProvider do
     state
     |> Enum.find({"-"}, fn {_key, val} -> Enum.member?(val, name) end)
     |> elem(0)
+    |> convert_string_date_to_mapped_date
   end
 
   defp get_init_content() do
@@ -56,5 +59,19 @@ defmodule Namedays.NamedayProvider do
   defp read_and_decode_json_file(file) do
     {:ok, namedaysContent} = File.read(file)
     Jason.decode!(namedaysContent)
+  end
+
+  defp convert_string_date_to_mapped_date(string_date) when is_bitstring(string_date) do
+      [month, day] = String.split(string_date, "-")
+
+      %{month: month, day: day}
+  end
+
+  defp get_namedays_by_listed_date([_month, _day] = date, state) do
+    date
+    |> Enum.map(fn value -> Integer.to_string(value) |> String.pad_leading(2, "0") end)
+    |> Enum.reduce(fn (elem, carry) -> carry<>"-"<>elem end)
+    |> (&Map.get(state, &1)).()
+    |> IO.inspect()
   end
 end
